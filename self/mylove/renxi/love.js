14aebc719fc86694d33ffe0cf06d1e6d5f1fee47
wanCheng7 <525918717@qq.com>
2018-03-19 22:42:06 +0800

    Site updated: 2018-03-19 22:41:48

--- self/mylove/renxi/love.js
+++ self/mylove/renxi/love.js
@@ -163,13 +163,13 @@
             ctx.scale(scale, scale);
             ctx.moveTo(0, 0);
     	    ctx.lineTo(15, 15);
-    	    ctx.lineTo(60, 15);
+    	    ctx.lineTo(70, 15);
             ctx.stroke();
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Come Baby", 23, 10);
+            ctx.fillText("点一下我的心", 23, 10);
             ctx.restore();
         },
         clear: function() {
